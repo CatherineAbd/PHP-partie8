16505d0d76c844eaf687114a834039a8da733418
Catherine <c.abdmeziem@laposte.net>
2021-01-21 09:31:19 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include "..\\top_html.php"; ?>
+  <h1>PHP PARTIE 8</h1>
+  <hr>
+  <ul>
+    <li><a href="Exo1/index.php">Exo 1</a></li>
+    <li><a href="Exo2/index.php">Exo 2</a></li>
+    <li><a href="Exo3/index.php">Exo 3</a></li>
+    <li><a href="Exo4/index.php">Exo 4</a></li>
+    <li><a href="Exo5/index.php">Exo 5</a></li>
+  </ul>
+  <hr>
+
+<?php 
+  include "..\bottom_html.php";
+?>
